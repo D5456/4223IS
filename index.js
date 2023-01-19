7e3769260a2a720c96eb6c62baaecffb5c009d88
Toño Velazquez <diegomv2307.a@gmail.com>
2023-01-19 10:56:16 -0600

    PROYECTO

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-alert ("hola");
-alert ("hola de nuevo");
-hola
-hola
-hola
+var alumno1= new Object;
+alumno1.matricula = 1322134204;
+alumno1.nombre = "pedro";
+alumno1.Apellidos = "Vazquez";
+alumno1.año_de_Nacimiento = "2004";
+alumno1.presentacion = function(){
+    let anios = 2023-this .anio_nacimiento;
+    let texto = "hola mi nombre es " +this.nombre +""+this.apellidos +",tengo"+ anios +"anos";
+    return texto;
+}
